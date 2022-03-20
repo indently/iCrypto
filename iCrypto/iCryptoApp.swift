943c46c2cb8dfb6152e1d4fc9efa9a151ef0ef69
Federico <codepalace@fastmail.se>
2022-03-20 12:49:46 +0100

    Initial Commit

---
+++ iCrypto/iCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCryptoApp.swift
+//  iCrypto
+//
+//  Created by Federico on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct iCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
